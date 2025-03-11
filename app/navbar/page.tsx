ff6401193eef0a4aac421d7af7d6f99ac55ed284
Brandon <brancarp93@gmail.com>
2025-03-11 12:00:05 -0400

    nav update

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,9 +1,18 @@
 "use client";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>Doors Direct South</h1>
+    <div className="bg-white border-b-2 sticky">
+      <div className="">
+      <Image className="p-3"
+      src="/images/logo.png"
+      alt="transparent logo"
+      width={150}
+      height={100}
+       />
+      </div>
+      
     </div>
   );
 }
