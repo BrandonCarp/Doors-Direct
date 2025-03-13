2cb0aea68cd5626037c1b6d0b82aa6c5524898d1
Brandon <brancarp93@gmail.com>
2025-03-13 16:13:00 -0400

    hero img update

---
+++ app/components/Hero.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+
+export default function Hero() {
+    return (
+        <div className="bg-[url('/images/4050hero.webp')] bg-cover bg-center h-screen">
+
+
+        </div>
+    )
+}
